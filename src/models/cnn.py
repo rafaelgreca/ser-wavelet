c6bbc9954f04b580840cb70d761cea6cc42c568a
rafaelgreca <rgvieira97@gmail.com>
2023-06-22 11:42:06 -0300

    removing mode 2 second implementation, rest of the pretrained and mode 3 scripts

--- src/models/cnn.py
+++ src/models/cnn.py
@@ -1,14 +1,16 @@
-from typing import Any
 import torch
 import torch.nn as nn
 from src.models.utils import weight_init
 
-class CNN_Mode2_Block(nn.Module):
+class CNN_Mode2(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.block = nn.Sequential(
+        self.input_channels = 5
+        self.linear_input_features = 119040
+        
+        self.model = nn.Sequential(
             nn.Conv2d(
-                in_channels=1,
+                in_channels=self.input_channels,
                 out_channels=64,
                 kernel_size=(2, 2)
             ),
@@ -27,51 +29,26 @@ class CNN_Mode2_Block(nn.Module):
             nn.MaxPool2d(
                 kernel_size=(2, 2)
             ),
-            nn.Flatten()
+            nn.Flatten(),
+            nn.Linear(
+                in_features=self.linear_input_features,
+                out_features=3
+            )
         )
 
-        self.block.apply(weight_init)
+        self.model.apply(weight_init)
         
     def forward(
         self,
         X: torch.Tensor
     ) -> torch.Tensor:
-        return self.block(X)
-    
-class CNN_Mode2(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-        self.models = nn.ModuleList(
-            [CNN_Mode2_Block() for _ in range(5)]
-        ) 
-        
-        self.output_layer = nn.Linear(
-            in_features=1190400,
-            out_features=3
-        )
-        
-        self.output_layer.apply(weight_init)
-    
-    def forward(
-        self,
-        X: torch.Tensor
-    ) -> torch.Tensor:
-        outputs = []
-        
-        for i, _ in enumerate(self.models):
-            output = self.models[i](X[:, i, :, :].unsqueeze(1)).unsqueeze(1)
-            outputs.append(output)
-            
-        outputs = torch.concat(outputs, dim=1)
-        outputs = torch.flatten(outputs, start_dim=1, end_dim=-1)
-        output = self.output_layer(outputs)
-        return output
-    
+        return self.model(X)
+
 class CNN_Mode1(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.input_channels = 1
-        self.linear_input_features = 147456
+        self.linear_input_features = 343040
             
         self.model = nn.Sequential(
             nn.Conv2d(
